12cba53c933ae892acf9dd8a46403a6902244f74
Hans <hans.steffens@gmail.com>
2019-03-01 21:43:36 -0500

    Sections: Ads, Services, Consulting

---
+++ wp-content/themes/parcemedia/sections/section-consulting.php
@@ -0,0 +1,41 @@
+<section class="slice slice-xl bg-gradient-primary has-floating-items" id="sct_call_to_action">
+    <a href="#sct_call_to_action" class="tongue tongue-up tongue-secondary scroll-me"><i class="fas fa-angle-up"></i></a>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-white strong-600">Social Media Consulting</h1>
+                <div class="row justify-content-center mt-4">
+                    <div class="col-lg-8">
+                        <p class="lead text-white">
+                          Starting with a detailed audit I will work collectively with your social media in-house team to set up goals, develop a strategic plan and execution to bring your social media channels to next level.
+                        </p>
+                        <div class="btn-container mt-5">
+                            <a href="#" target="_blank" class="btn btn-white btn-circle btn-translate--hover px-4 mr-lg-4">Learn more</a>
+                            <a href="#" target="_blank" class="btn btn-warning btn-circle btn-translate--hover px-4">Let's work together</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container floating-items">
+        <div class="floating-icon"><span></span>
+            <img alt="" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/dusk/svg/add.svg" class="img-saturate" style="width: 50px;">
+        </div>
+        <div class="floating-icon"><span></span>
+            <img alt="" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/dusk/svg/change-theme.svg" class="img-saturate" style="width: 76px;">
+        </div>
+        <div class="floating-icon"><span></span>
+            <img alt="" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/dusk/svg/comments.svg" class="img-saturate" style="width: 36px;">
+        </div>
+        <div class="floating-icon"><span></span>
+            <img alt="" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/dusk/svg/download.svg" class="img-saturate" style="width: 76px;">
+        </div>
+        <div class="floating-icon"><span></span>
+            <img alt="" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/dusk/svg/form.svg" class="img-saturate" style="width: 46px;">
+        </div>
+        <div class="floating-icon"><span></span>
+            <img alt="" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/dusk/svg/ios-photos.svg" class="img-saturate" style="width: 36px;">
+        </div>
+    </div>
+</section>
---
+++ wp-content/themes/parcemedia/sections/section-services.php
@@ -0,0 +1,62 @@
+<section id="sct_page_examples" class="slice">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 mb-5 mb-lg-0">
+                <div data-animate-hover="1" data-toggle="hidden">
+                    <div class="card bg-gradient-primary shadow-primary border-primary animate-this">
+                        <div class="py-5 text-center">
+                            <img alt="Image placeholder" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/dusk/svg/comments.svg" class="img-saturate" style="width: 100px;">
+                        </div>
+                        <div class="px-4 pb-5 text-center">
+                          <h2 class="text-white">Services</h2>
+                            <!--<a href="<?php echo get_template_directory_uri(); ?>/pages/pages-landing.html" class="btn btn-sm btn-white btn-circle">Social Media</a>-->
+                            <p class="text-white mt-4">Let's get started with one of these landing page examples.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 mb-5 mb-lg-0">
+                <div data-animate-hover="1" data-toggle="hidden">
+                    <div class="card shadow animate-this">
+                        <div class="py-5 text-center">
+                            <img alt="" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/dusk/svg/details-popup.svg" class="img-saturate" style="width: 100px;">
+                        </div>
+                        <div class="px-4 pb-5 text-center">
+                            <a href="<?php echo get_template_directory_uri(); ?>/pages/pages-secondary.html" class="btn btn-sm btn-dark btn-circle">Social Media Coaching</a>
+                            <p class="mt-4">Inner pages templates designed to cover a large number of purposes.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 mb-5 mb-lg-0">
+                <div data-animate-hover="1" data-toggle="hidden">
+                    <div class="card shadow animate-this">
+                        <div class="py-5 text-center">
+                            <img alt="" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/dusk/svg/list.svg" class="img-saturate" style="width: 100px;">
+                        </div>
+                        <div class="px-4 pb-5 text-center">
+                            <a href="<?php echo get_template_directory_uri(); ?>/pages/pages-account.html" class="btn btn-sm btn-dark btn-circle">Social Media Consulting</a>
+                            <p class="mt-4">Pages designed for user registration, account administration and more.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 mb-5 mb-lg-0">
+                <div data-animate-hover="1" data-toggle="hidden">
+                    <div class="card shadow animate-this">
+                        <div class="py-5 text-center">
+                            <img alt="" src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/dusk/svg/in-progress.svg" class="img-saturate" style="width: 100px;">
+                        </div>
+                        <div class="px-4 pb-5 text-center">
+                            <a href="<?php echo get_template_directory_uri(); ?>/pages/pages-shop.html" class="btn btn-sm btn-dark btn-circle">Social Ads Management</a>
+                            <p class="mt-4">A complete e-commerce flow to integrate your online shop fast and easy.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="fluid-paragraph text-center mt-5">
+            <p><strong class="text-primary">*Good to know!</strong> You are not limited to the examples we've built. We have the liberty to choose any of the page layouts and components from the package and create your own version.</p>
+        </div>
+    </div>
+</section>
