05f128acdf97e984241a2255c472909ed8926e6d
Hans <hans.steffens@gmail.com>
2019-03-08 00:18:53 -0500

    Blog posts Query Improved

--- wp-content/themes/parcemedia/sections/section-blog.php
+++ wp-content/themes/parcemedia/sections/section-blog.php
@@ -1,41 +1,41 @@
+<?php
+  $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+  $args = array(
+    'post_type'      => 'post',
+    'posts_per_page' => 3,
+    'paged'          => $paged
+  );
+  $query = new WP_Query($args);
+
+?>
+
 <section class="slice slice-lg bg-secondary">
-    <div class="container" id="ajax">
-      <?php
 
-            $posts = get_posts(array(
-                'posts_per_page'	=> -1,
-                'post_type'			=> 'post'
-            ));
+  <div class="container" id="ajax">
 
-            if ($posts): ?>
+      <div class="row row-grid">
 
-            	<div class="row row-grid">
+        <?php if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
 
-            	<?php foreach ($posts as $post):
-
-                    setup_postdata($post);
-
-                    ?>
-                    <div class="col-lg-4 mb-5">
-                      <div class="card shadow-sm shadow--hover">
-                          <img alt="Image placeholder" src="<?php echo esc_url(get_the_post_thumbnail_url(get_the_ID(),'full')); ?>" class="card-img-top">
-                          <div class="card-body py-5 text-center">
-                              <a href="<?php the_permalink(); ?>" class="h5 lh-150"><?php the_title(); ?></a>
-                              <h6 class="text-muted mt-4 mb-0"><?php echo get_the_date( 'F j, Y' ); ?></h6>
-                          </div>
-                      </div>
+            <div class="col-lg-4 mb-5">
+              <div class="card shadow-sm shadow--hover">
+                  <a href="<?php the_permalink(); ?>"><img alt="Image placeholder" src="<?php echo esc_url(get_the_post_thumbnail_url(get_the_ID(), 'full')); ?>" class="card-img-top"></a>
+                  <div class="card-body py-5 text-center">
+                      <a href="<?php the_permalink(); ?>" class="h5 lh-150"><?php the_title(); ?></a>
+                      <h6 class="text-muted mt-4 mb-0"><?php echo get_the_date('F j, Y'); ?></h6>
                   </div>
+              </div>
+          </div>
 
-            	<?php endforeach; ?>
+          <?php endwhile; endif; wp_reset_postdata(); ?>
 
-            	</div><!--/.row -->
+      </div><!--/.row -->
 
-            	<?php wp_reset_postdata(); ?>
-
-    <?php endif; ?>
+  </div><!--/.container-->
     <!-- Load more -->
     <div class="mt-lg text-center">
+      <?php load_more_button(); ?>
         <!--<a href="#" class="btn btn-dark btn-circle shadow">Load more</a>-->
     </div>
-    </div><!--/.container-->
 </section>
